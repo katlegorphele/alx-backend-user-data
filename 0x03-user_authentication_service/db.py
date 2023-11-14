3f88346ed35d965556dc7eea0e8e2217f401053a
Kat-Devs <katlegophele95@gmail.com>
2023-11-15 01:29:19 +0200

    0x03 - find_user_by function failing

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,78 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import User
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+    
+    # Implement add user method, takes in email and hashed_password
+    # and returns a User object
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a user to the database
+        """
+        new_user = User()
+        new_user.email = email
+        new_user.hashed_password = hashed_password
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+    
+    # Implement find user method, takes in email and returns the
+    # first row found in the users table as filtered by email. If
+    # no row is found, returns None
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by a given attribute
+        """
+
+        # if not kwargs:
+        #     raise InvalidRequestError
+
+        # if not all(
+        #     hasattr(User, k) for k in kwargs.keys()
+        # ):
+        #     raise InvalidRequestError
+
+        # user = self._session.query(User).filter_by(**kwargs).first()
+
+        # if user is None:
+        #     raise NoResultFound
+
+        # return user
+        for k in kwargs.keys():
+            if k not in User.__dict__.keys():
+                raise InvalidRequestError
+        query = self._session.query(User).filter_by(**kwargs)
+        # get the first row of user
+        user = query.first()
+        if user is None:
+            raise NoResultFound
+        return user
