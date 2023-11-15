9bad37ba890b7115903471b7d9cd4d2b8167da20
Kat-Devs <katlegophele95@gmail.com>
2023-11-15 21:07:52 +0200

    echo fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -18,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -38,9 +38,9 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database
         """
-        new_user = User()
-        new_user.email = email
-        new_user.hashed_password = hashed_password
+        new_user = User(email=email, hashed_password=hashed_password)
+        #new_user.email = email
+        #new_user.hashed_password = hashed_password
         self._session.add(new_user)
         self._session.commit()
         return new_user
